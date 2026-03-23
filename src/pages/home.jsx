ba59249f21024323669a7eb18ed946f579d129cc
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-23 17:42:00 +0100

    home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,22 @@
 export default function home(){
     return(
         <>
-          <h1>BENVENUTO!</h1>
-          <img src="https://nuvoluzione.com/wp-content/uploads/2016/03/vendita-promozionale.jpg" alt="img promozionale" />
-        </>
+        <header> 
+            <a href="/">Home</a>
+            <a href="/presentazione">chi siamo</a>
+            <a href="products">prodotti</a>
+        </header>
+
+        <main>
+            <h1>BENVENUTO!</h1>
+            <img src="https://nuvoluzione.com/wp-content/uploads/2016/03/vendita-promozionale.jpg" alt="img promozionale" />
+
+        </main>
+
+        <footer>
+            <h2>fine</h2>
+        </footer>
+          
+          </>
     )
 }
