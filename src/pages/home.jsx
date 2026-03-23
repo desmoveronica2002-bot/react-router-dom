10e6814f276f7c9e200712685eb23f5beaa7b748
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-23 20:01:29 +0100

    navbar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,23 @@ export default function Home(){
     return(
         <>
         <header> 
-            <a href="/">Home</a>
-            <a href="/presentazione">chi siamo</a>
-            <a href="products">prodotti</a>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+               <div class="container-fluid">
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                   <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/presentazione">chi siamo</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="products">prodotti</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </nav>
         </header>
 
         <main>
