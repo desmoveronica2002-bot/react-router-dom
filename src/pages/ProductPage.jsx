5fdfacbca972d14c52dbe6b8ab59eb88649021e1
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-24 16:48:54 +0100

    productPage continue

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { data, useParams } from "react-router-dom"
 
 export default function ProductPage(){
 
     const {id} = useParams()
     console.log(id);
 
+    const api_url = `https://fakestoreapi.com/products/${id}`;
+    console.log(api_url);
+
     useEffect(()=>{
+        fetch(api_url)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+        })
 
     },[])
 
