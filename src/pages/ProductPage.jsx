82556cd66826b2cf3ff54e211819f6e94ad3d3e3
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-24 17:10:43 +0100

    continue p4 productPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { data, useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom";
 
 export default function ProductPage(){
 
@@ -23,6 +23,27 @@ export default function ProductPage(){
     return(
         <>
 
+        <header className="container"> 
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+               <div className="container-fluid">
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                   <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="nav-link" to="/presentazione">chi siamo</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="nav-link" to="/products">prodotti</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </nav>
+        </header>
+
+        <main className="container">
             {product!==null &&(
                 <div>
                     <img src={product.image}></img>
@@ -30,8 +51,9 @@ export default function ProductPage(){
 
                 </div>
 
-
             )}
+
+        </main>
         </>
     )
 }
